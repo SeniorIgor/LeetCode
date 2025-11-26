b29b397bf9a9382fab619302a796e6f04de9b319
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2025-11-26 09:15:28 +0600

    feat: solved 80. Remove Duplicates from Sorted Array II

--- challenges/80. Remove Duplicates from Sorted Array II.ts
+++ challenges/80. Remove Duplicates from Sorted Array II.ts
@@ -1,15 +1,40 @@
-// TODO: Solve again
 export function removeDuplicates(nums: number[]): number {
-  let index = 0;
+  let start = 0;
+  let result = 1;
 
-  for (let i = 0; i < nums.length; i++) {
-    if (index < 2 || nums[index - 2] !== nums[i]) {
-      nums[index++] = nums[i];
+  for(let i = 1; i < nums.length; i++) { 
+    if(nums[start] !== nums[i]) {
+      nums[result] = nums[i];
+      result++;
+      start = i;
+    } else if(i - start < 2) {
+      nums[result] = nums[i];
+      result++;
     }
   }
 
-  return index;
+  return result;
 }
 
 const nums = [0,0,1,1,1,1,2,3,3];
 console.log(removeDuplicates(nums), nums);
+
+// [0,0,0,0,1,1,1,1,2,3,3,3,4]
+// [0,0,0,0,1,1,1,1,2,3,3,3,4]
+//S |
+//i   |
+//R   |
+
+// Previous Solution
+// TODO: Solve again
+// export function removeDuplicates(nums: number[]): number {
+//   let index = 0;
+
+//   for (let i = 0; i < nums.length; i++) {
+//     if (index < 2 || nums[index - 2] !== nums[i]) {
+//       nums[index++] = nums[i];
+//     }
+//   }
+
+//   return index;
+// }
