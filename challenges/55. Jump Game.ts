8391b647ea7337bd25399e2d16449217a2b21a31
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2025-12-10 07:00:15 +0600

    feat: solved 55. Jump Game

--- challenges/55. Jump Game.ts
+++ challenges/55. Jump Game.ts
@@ -1,17 +1,17 @@
-function canJump(nums: number[]): boolean {
+export function canJump(nums: number[]): boolean {
   let index = 0;
 
-  for (let i = 0; i < nums.length; i++) {
+  for(let i = 0; i < nums.length; i++) {
     if(index < i) {
       return false;
+    } else if(nums[i] + i > index) {
+      index = nums[i] + i;
     }
-    
-    index = Math.max(index, nums[i] + i);
   }
 
   return index >= nums.length - 1;
 };
 
-const example = [3,2,1,0,4];
+const example = [2,2,0,1,4];
 
 console.log(canJump(example));
