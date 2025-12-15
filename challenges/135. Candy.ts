93daea970d2ae3f7dd808ab46790a2e47e62ae21
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2025-12-15 12:12:12 +0600

    feat: solved 135. Candy

--- challenges/135. Candy.ts
+++ challenges/135. Candy.ts
@@ -1,4 +1,5 @@
-function candy(ratings: number[]): number {
+// TODO: Solve again
+export function candy(ratings: number[]): number {
   let candies = ratings.length;
   let down = 0;
   let up = 0;
@@ -9,9 +10,7 @@ function candy(ratings: number[]): number {
     
     if(difference < 0) {
       up = 0;
-      down++;
-
-      
+      down += 1;
       candies += down + (peak >= down ? -1 : 0);
     } else if (difference === 0) {
       down = 0;
@@ -23,13 +22,47 @@ function candy(ratings: number[]): number {
       peak = up;
       candies += up;
     }
-
-    console.log({ peak, up, down, candies });
   }
 
   return candies;
 };
 
-const value = [1,3,2,2,1];
+// Test Case #1 -> 7
+// const value = [1,3,2,2,1];
+// Test Case #2 -> 5
+// const value = [1,0,2];
+// Test Case #3 -> 4
+// const value = [1,2,2];
+// Test Case #4 -> 11
+// const value = [1,3,4,5,2];
+// Test Case #5 -> 9
+// const value = [1,2,3,1,0];
+// Test Case #6 -> 18
+// const value = [1,6,10,8,7,3,2];
+// Test Case #7 -> 15
+const value = [0,1,2,5,3,2,7];
+
 const result = candy(value);
 console.log(result);
+
+// Solution with O(n) additional memory use
+// export function candy(ratings: number[]): number {
+//   let candies: Array<number> = [];
+//   candies[0] = 1;
+
+//   for(let i = 1; i < ratings.length; i++) {
+//     if(ratings[i] > ratings[i - 1]) {
+//       candies[i] = candies[i - 1] + 1;
+//     } else {
+//       candies[i] = 1;
+//     }
+//   }
+
+//   for(let i = ratings.length - 2; i >= 0; i--) {
+//     if(ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {  
+//       candies[i] = candies[i + 1] + 1;
+//     }
+//   }
+
+//   return candies.reduce((candy, result) => result + candy, 0);
+// };
