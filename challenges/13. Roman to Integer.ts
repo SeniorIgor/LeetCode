ab61e5cc873412d012a87873acb95bf2f99a46c0
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2025-12-17 07:36:34 +0600

    feat: solved 13. Roman to Integer

--- challenges/13. Roman to Integer.ts
+++ challenges/13. Roman to Integer.ts
@@ -1,18 +1,17 @@
-function romanToInt(s: string): number {
-  const map = new Map([['I', 1], ["V", 5], ['X', 10], ["L", 50], ["C", 100], ["D", 500], ["M", 1000]]);
+export function romanToInt(s: string): number {
+  const map = new Map([["I", 1], ["V", 5], ["X", 10], ["L", 50],  ["C", 100], ["D", 500], ["M", 1000]]);
 
-  const result = s.replace(/IV/g, 'IIII')
-   .replace(/IX/g, "VIIII")
-   .replace(/XL/g, "XXXX")
-   .replace(/XC/g, 'LXXXX')
-   .replace(/CD/g, 'CCCC')
-   .replace(/CM/g, "DCCCC");
+  s = s.replace(/IV/g, 'IIII')
+       .replace(/IX/g, 'VIIII')
+       .replace(/XL/g, 'XXXX')
+       .replace(/XC/g, 'LXXXX')
+       .replace(/CD/g, 'CCCC')
+       .replace(/CM/g, 'DCCCC');
 
-  const sum = result.split('').reduce((res, item) => res += (map.get(item) || 0), 0);
-
-  return sum;
+  return s.split('').reduce((result, value) => result + (map.get(value) || 0), 0);
 };
 
+// Test Case #1 -> 1994
 const value = "MCMXCIV";
 const result = romanToInt(value);
 console.log(result);
