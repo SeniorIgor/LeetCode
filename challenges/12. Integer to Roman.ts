047d9508560e71c0b4295e69bd671d3ae664d087
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2025-12-18 08:57:11 +0600

    feat: solved 12. Integer to Roman

--- challenges/12. Integer to Roman.ts
+++ challenges/12. Integer to Roman.ts
@@ -1,15 +1,22 @@
-function intToRoman(num: number): string {
-  const thousands = ['', "M", "MM", "MMM", "MMMM"];
-  const hundreds = ['', 'C', 'CC', "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
-  const tens = ['', 'X', "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]; 
-  const digits = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+export function intToRoman(num: number): string {
+  const thousands = ["", "M", "MM", "MMM", "MMMM"];
+  const hundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+  const tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+  const units = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
 
-  const result = thousands[Math.floor(num / 1000)] + hundreds[Math.floor(num % 1000 / 100)] + 
-    tens[Math.floor(num % 100 / 10)] + digits[Math.floor(num % 10)];
-
-  return result;
+  return thousands[Math.trunc(num / 1000)] 
+    + hundreds[Math.trunc(num % 1000 / 100)] 
+    + tens[Math.trunc(num % 100 / 10)]
+    + units[Math.trunc(num % 10)];
 };
 
-const value = 1994;
+// Test Case #1 -> MCMXCIV
+// const value = 1994;
+// Test Case #2 -> MMMDCCXLIX
+// const value = 3749;
+// Test Case #3 -> LVIII
+// const value = 58;
+// Test Case #4 -> X
+const value = 10;
 const result = intToRoman(value);
 console.log(result);
