a3fcacc3a0e8a58d09e17c62f2d56b5a2c52869e
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2025-12-19 09:46:53 +0600

    feat: solved 58. Length of Last Word

--- challenges/58. Length of Last Word.ts
+++ challenges/58. Length of Last Word.ts
@@ -1,10 +1,16 @@
-function lengthOfLastWord(s: string): number {
-  const trimmedString = s.trimEnd();
-  const lastWordIndex = trimmedString.lastIndexOf(' ');
-
-  return trimmedString.length - lastWordIndex - 1;
+export function lengthOfLastWord(s: string): number {
+  const str = s.trim();
+  
+  return str.length - 1 - str.lastIndexOf(" ");
 };
 
 const value = "   fly me   to   the moon  ";
 const result = lengthOfLastWord(value);
 console.log(result);
+
+// Solution #2
+// export function lengthOfLastWord(s: string): number {
+//   const array = s.trim().split(' ');
+  
+//   return array[array.length - 1].length;
+// };
