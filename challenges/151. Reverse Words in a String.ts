8e8d3a8bcb0d6466c5ecd2595502251e6d3df106
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2025-12-21 07:14:38 +0600

    feat: solved 151. Reverse Words in a String

--- challenges/151. Reverse Words in a String.ts
+++ challenges/151. Reverse Words in a String.ts
@@ -1,7 +1,12 @@
-function reverseWords(s: string): string {
-  return s.trim().split(/\s+/).reverse().join(' ');
+export function reverseWords(s: string): string {
+  return s.match(/\S+/g)?.reverse().join(" ") || "";
 };
 
 const value = "the sky is blue";
 const result = reverseWords(value);
 console.log(result);
+
+// Solution #1
+// function reverseWords(s: string): string {
+//   return s.trim().split(/\s+/).reverse().join(' ');
+// };
