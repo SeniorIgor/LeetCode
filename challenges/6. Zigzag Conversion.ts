d016d32d508357f3665680b26259d4b90ae935c2
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2025-12-22 08:54:24 +0600

    feat: solved 6. Zigzag Conversion

--- challenges/6. Zigzag Conversion.ts
+++ challenges/6. Zigzag Conversion.ts
@@ -1,37 +1,96 @@
-function convert(s: string, numRows: number): string {
-  if(numRows < 2) {
+export function convert(s: string, numRows: number): string {
+  if(s.length < 2 || numRows === 1 || numRows > s.length) {
     return s;
   }
   
-  const separator = numRows - 2 + numRows;
-  let result = '';
+  let result = "";
   
-  for (let i = 0; i < numRows; i++) {
-    let index = i;
-    let rest = 0;
+  for(let row = 1; row <= numRows; row++) {
+    let index = row;
+    let zigzagNumber = 1;
     
-    while(index < s.length) {
-      result += s[index];
-      const lastRow = numRows - 1;
-      const isFirstLastRow = i === 0 || i === lastRow;
+    while(index <= s.length) {
+      result += s[index - 1];
 
-      if(isFirstLastRow) {
-        index += separator;
+      // Side rows
+      if(row === 1 || row === numRows) {
+        index += (numRows - 1) * 2;
       } else {
-        const shift = 2 * (lastRow - i);
-        
-        index += rest > 0 ? separator - rest : shift;
-        rest = rest > 0 ? 0 : shift; 
+        if(zigzagNumber % 2 === 1) {
+          index += (numRows - row) * 2;
+        } else {
+          index += ((numRows - 1) - (numRows - row)) * 2;
+        }
       }
+
+      zigzagNumber++;
     }
   }
 
   return result;
 };
 
-const value = "ABCD";
-const numRows = 2;
+// Test Case #1 -> ACBD
+// const value = "ABCD";
+// const numRows = 2;
+
+// Test Case #2 -> PAHNAPLSIIGYIR
+// const value = "PAYPALISHIRING";
+// const numRows = 3;
+
+// Test Case #3 -> PINALSIGYAHRPI
+// const value = "PAYPALISHIRING";
+// const numRows = 4;
+
+// Test Case #4 -> A
+// const value = "A";
+// const numRows = 1;
+
+// Test Case #5 -> AB
+const value = "AB";
+const numRows = 3;
+
 const result = convert(value, numRows);
 console.log(result);
 
 
+// 5 rows
+// 1 row: 4 + 4
+// 2 row: 3 + 3  ->  1 + 1
+// 3 row: 2 + 2
+// 4 row: 1 + 1  ->  3 + 3
+// 5 row: 4 + 4
+// 
+
+// 4 rows
+// 1 row: 3 + 3
+// 2 row: 2 + 2  ->  1 + 1
+// 3 row: 1 + 1  ->  2 + 2
+// 4 row: 3 + 3
+// 
+
+// 6 rows
+// 1 row: 5 + 5
+// 2 row: 4 + 4  ->  1 + 1
+// 3 row: 3 + 3  ->  2 + 2
+// 4 row: 2 + 2  ->  3 + 3
+// 5 row: 1 + 1  ->  4 + 4
+// 6 row: 4 + 4
+
+// 1 Q          O
+// 2 W        I P
+// 3 E      U   A       G
+// 4 R    Y     S     F
+// 5 T  V       D  C
+// 6 Z          X
+
+// 1 Q        O
+// 2 W      I P
+// 3 E    U   A    G
+// 4 R  Y     S  F
+// 5 T        D
+
+// 1 Q      O
+// 2 W    I P
+// 3 E  U   A  G
+// 4 R      S
