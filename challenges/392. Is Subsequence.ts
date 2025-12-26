cebab4472ec1f5e659a5afe1556763a12c6738d1
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2025-12-26 12:21:57 +0600

    feat: solved 392. Is Subsequence

--- challenges/392. Is Subsequence.ts
+++ challenges/392. Is Subsequence.ts
@@ -1,16 +1,57 @@
-function isSubsequence(s: string, t: string): boolean {
-  let pointer = 0;
-
-  for (let i = 0; i < t.length; i++) {
-    if(t[i] === s[pointer]) {
-      pointer++;
+export function isSubsequence(s: string, t: string): boolean {
+  let sIndex = 0;
+  let tIndex = 0;
+  
+  while(sIndex < s.length && tIndex < t.length) {
+    if(s[sIndex] !== t[tIndex]) {
+      tIndex++;
+    } else {
+      tIndex++;
+      sIndex++;
     }
   }
 
-  return pointer === s.length;
+  return sIndex === s.length;
 };
 
+// Test Case #1 -> false
 const subsequence = "axc";
 const value = "ahbgdc"
 const result = isSubsequence(subsequence, value);
 console.log(result);
+
+// First Solution
+// export function isSubsequence(s: string, t: string): boolean {
+//   let prevIndex = 0;
+  
+//   for(let i = 0; i < s.length; i++) {
+//     let isLetterExist = false;
+
+//     for (let j = prevIndex; j < t.length; j++) {
+//       if(s[i] === t[j]) {
+//         isLetterExist = true;
+//         prevIndex = j + 1;
+//         break;
+//       }
+//     }
+
+//     if(!isLetterExist) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// };
+
+// Second Solution
+// function isSubsequence(s: string, t: string): boolean {
+//   let pointer = 0;
+
+//   for (let i = 0; i < t.length; i++) {
+//     if(t[i] === s[pointer]) {
+//       pointer++;
+//     }
+//   }
+
+//   return pointer === s.length;
+// };
