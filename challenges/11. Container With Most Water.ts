d5c08c4a7568d4c35327108e60f6d93b2830a59e
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2025-12-29 15:54:46 +0300

    feat: solved 11. Container With Most Water

--- challenges/11. Container With Most Water.ts
+++ challenges/11. Container With Most Water.ts
@@ -1,27 +1,29 @@
-function maxArea(height: number[]): number {
-  let start = 0;
-  let end = height.length - 1;
+export function maxArea(height: number[]): number {
+  let first = 0;
+  let last = height.length - 1;
+  let max = 0;
 
-  let sum = 0;
+  while(first < last) {
+    const minHight = Math.min(height[first], height[last]);
+    const currentMax = minHight * (last - first);
 
-  while (start < end) {
-    const distance = end - start;
-    const minHeight = Math.min(height[start], height[end]); 
-    
-    if(distance * minHeight > sum) {
-      sum = distance * minHeight;
+    if(currentMax > max) {
+      max = currentMax;
     }
 
-    if(height[start] > height[end]) {
-      end--
+    if(height[first] > height[last]) {
+      last--;
     } else {
-      start++;
+      first++;
     }
   }
 
-  return sum;
-};
+  return max;
+}
 
-const value = [1,1];
+// Test Case #1
+// const value = [1,1];
+// Test Case #2
+const value = [1,8,6,2,5,4,8,3,7];
 const result = maxArea(value);
 console.log(result);
