d7c121d30686cf64327ce169988ac5642a8452ef
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2026-01-13 08:43:36 +0600

    feat: solved 209. Minimum Size Subarray Sum

--- challenges/209. Minimum Size Subarray Sum.ts
+++ challenges/209. Minimum Size Subarray Sum.ts
@@ -1,23 +1,31 @@
-// TODO: Solve again
-function minSubArrayLen(target: number, nums: number[]): number {
-  let length = Number.MAX_SAFE_INTEGER;
-  let start = 0;
-  let end = 0;
+export function minSubArrayLen(target: number, nums: number[]): number {
+  let first = 0;
+  let last = 0;
   let sum = 0;
+  let minLength = Number.MAX_SAFE_INTEGER;
 
-  while(end < nums.length) {
-    sum += nums[end++];
+  while(last < nums.length) {
+    sum += nums[last];
+    last++;
 
     while(sum >= target) {
-      length = Math.min(length, end - start);
-      sum -= nums[start++];
+      minLength = Math.min(minLength, last - first);
+      sum -= nums[first];
+      first++;
     }
   }
 
-  return length === Number.MAX_SAFE_INTEGER ? 0 : length;
+  return minLength === Number.MAX_SAFE_INTEGER ? 0 : minLength;
 };
 
-const value = [2,3,1,2,4,3];
-const target = 7;
+// Test Case #1
+// const value = [2,3,1,2,4,3];
+// const target = 7;
+// Test Case #2
+// const value = [1,1,1,1,1,1,1,1];
+// const target = 11;
+// Test Case #3
+const value = [5,1,3,5,10,7,4,9,2,8];
+const target = 15;
 const result = minSubArrayLen(target, value);
 console.log(result);
