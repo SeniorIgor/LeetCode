9040be0b5f12f27b45f894d1f687cca141e5b6bd
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2026-01-15 08:17:16 +0600

    feat: solved 3. Longest Substring Without Repeating Characters

--- challenges/3. Longest Substring Without Repeating Characters.ts
+++ challenges/3. Longest Substring Without Repeating Characters.ts
@@ -1,28 +1,29 @@
-function lengthOfLongestSubstring(s: string): number {
-  const map = new Map();
-  let length = 0;
-  let start = 0;
+export function lengthOfLongestSubstring(s: string): number {
+  const symbols = new Map();
+  let maxLength = 0;
+  let first = 0;
 
-  for (let i = 1; i <= s.length; i++) { 
-    const element = s[i - 1];
-    const elementPointer = map.get(element);
-
-    if(elementPointer !== undefined) {
-      length = Math.max(i - 1 - start, length);
-      start = Math.max(start, elementPointer);
-    } 
+  for(let i = 0; i < s.length; i++) {
+    const position = symbols.get(s[i]);
     
-    map.set(element, i);
+    if(position !== undefined) {
+      maxLength = Math.max(maxLength, i - first);
+      first = Math.max(position + 1, first);
+    }
+    
+    symbols.set(s[i], i);
   }
 
-  return Math.max(s.length - start, length);
+  return Math.max(maxLength, s.length - first);
 };
 
-const value = " ";
+// Test Case #1
+// const value = "abcabcbb";
+// Test Case #2
+// const value = " ";
+// Test Case #3
+// const value = "dvdf";
+// Test Case #4
+const value = "abba";
 const result = lengthOfLongestSubstring(value);
 console.log(result);
-
-// "abba"
-// "dvdk"
-//   |
-//  1234
