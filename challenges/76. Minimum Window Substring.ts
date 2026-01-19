8cc1df5a0ad2bc27e809b5a8f282b18ea8a841b2
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2026-01-19 10:35:37 +0600

    feat: solved 76. Minimum Window Substring

--- challenges/76. Minimum Window Substring.ts
+++ challenges/76. Minimum Window Substring.ts
@@ -1,50 +1,101 @@
 // TODO: Solve again
-function minWindow(s: string, t: string): string {
-  const map = new Map();
-  let start = 0;
-  let minStart = 0;
-  let minLength = Number.MAX_SAFE_INTEGER;
-  let counter = t.length;
+export function minWindow(s: string, t: string): string {
+	const freq = new Map();
 
-  for (let i = 0; i < t.length; i++) {
-    map.set(t[i], (map.get(t[i]) || 0) + 1);    
-  }
+	for (let letter of t) {
+		freq.set(letter, (freq.get(letter) || 0) + 1);
+	}
 
-  for (let end = 1; end <= s.length; end++) {
-    const value = map.get(s[end - 1]);
-    
-    if(value !== undefined) {
-      if(value > 0) {
-        counter--;
-      }
+	let left = 0;
+	let counter = 0;
+	let result = '';
+	let subLen = Number.MAX_SAFE_INTEGER;
 
-      map.set(s[end - 1], value - 1);
+	for (let right = 0; right < s.length; right++) {
+		const letter = s[right];
+		const count = freq.get(letter);
 
-      while(counter === 0) {
-        if(end - start < minLength) {
-          minLength = end - start;
-          minStart = start;
-        }
-        
-        const innerValue = map.get(s[start]);
-        
-        if(innerValue !== undefined) {
-          if(innerValue + 1 > 0) {
-            counter++;
-          }
-          
-          map.set(s[start], innerValue + 1);
-        }
-        
-        start++;
-      }
-    }
-  }
+		if (count !== undefined) {
+			freq.set(letter, count - 1);
 
-  return minLength === Number.MAX_SAFE_INTEGER ? "" : s.slice(minStart, minStart + minLength);
-};
+			if (count > 0) {
+				counter++;
+			}
 
-const value = "cabwefgewcwaefgcf";
-const words = "cae";
+			while (!freq.has(s[left]) || freq.get(s[left]) < 0) {
+				if (freq.has(s[left])) {
+					freq.set(s[left], freq.get(s[left]) + 1);
+				}
+
+				left++;
+			}
+
+			if (counter === t.length && right + 1 - left < subLen) {
+				subLen = right + 1 - left;
+				result = s.slice(left, right + 1);
+			}
+		}
+	}
+
+	return result;
+}
+
+// Test Case #1 -> BANC
+// const value = 'ADOBECODEBANC';
+// const words = 'ABC';
+// Test Case #2 -> baca
+// const value = 'acbbaca';
+// const words = 'aba';
+// Test Case #3 -> abbbbbcdd
+// const value = 'aaaaaaaaaaaabbbbbcdd';
+// const words = 'abcdd';
+// Test Case #4 -> ab
+// const value = 'bdab';
+// const words = 'ab';
+// Test Case #5 -> ""
+const value = 'a';
+const words = 'b';
 const result = minWindow(value, words);
-console.log(result);
+console.log({ result });
+
+// Another Solution
+// export function minWindow(s: string, t: string): string {
+// 	const freq = new Map();
+
+// 	for (let letter of t) {
+// 		freq.set(letter, (freq.get(letter) || 0) + 1);
+// 	}
+
+// 	let left = 0;
+// 	let bestStart = 0;
+// 	let matched = 0;
+// 	let minLen = Number.MAX_SAFE_INTEGER;
+
+// 	for (let right = 0; right < s.length; right++) {
+// 		const letter = s[right];
+
+// 		if (freq.has(letter)) {
+// 			const count = freq.get(letter);
+// 			freq.set(letter, count - 1);
+
+// 			if (count > 0) {
+// 				matched++;
+// 			}
+
+// 			while (!freq.has(s[left]) || freq.get(s[left]) < 0) {
+// 				if (freq.has(s[left])) {
+// 					freq.set(s[left], freq.get(s[left]) + 1);
+// 				}
+
+// 				left++;
+// 			}
+
+// 			if (matched === t.length && right + 1 - left < minLen) {
+// 				minLen = right + 1 - left;
+// 				bestStart = left;
+// 			}
+// 		}
+// 	}
+
+// 	return minLen === Number.MAX_SAFE_INTEGER ? '' : s.slice(bestStart, bestStart + minLen);
+// }
