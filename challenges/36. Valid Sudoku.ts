4b837afcf476f202daa5726adc8581a5eebd9558
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2026-01-20 09:30:19 +0600

    feat: solved 36. Valid Sudoku

--- challenges/36. Valid Sudoku.ts
+++ challenges/36. Valid Sudoku.ts
@@ -1,45 +1,58 @@
 // TODO: Solve again
-function isValidSudoku(board: string[][]): boolean {
-  let rowSets: Array<Set<string>> = [];
-  let columnSets: Array<Set<string>> = [];
-  let blockSets: Array<Set<string>> = [];
+export function isValidSudoku(board: string[][]): boolean {
+  const cols = Array.from({ length: 9 }, () => new Set<string>());
+  const boxes = Array.from({ length: 9 }, () => new Set<string>());
 
-  for (let i = 0; i < board.length; i++) {
-    rowSets[i] = new Set();    
-    columnSets[i] = new Set();    
-    blockSets[i] = new Set();    
-  }
-  
+  // Row
   for (let i = 0; i < 9; i++) {
-    for (let j = 0; j < 9; j++) {
-      const blockKey = Math.floor(i / 3) * 3 + Math.floor(j / 3);
-      
-      if(board[i][j] !== ".") {
-        const innerItem = board[i][j];
+    const row = new Set<string>();
 
-        if(rowSets[i].has(innerItem) || columnSets[j].has(innerItem) || blockSets[blockKey].has(innerItem)) {
-          return false;
-        } else {
-          rowSets[i].add(innerItem);
-          columnSets[j].add(innerItem);
-          blockSets[blockKey].add(innerItem);
-        }
+    for (let j = 0; j < 9; j++) {
+      const val = board[i][j];
+
+      // Filter not numbers
+      if (val === ".") {
+        continue;
       }
-    }    
+
+      const boxIndex = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+
+      // Check repeated value
+      if (row.has(val) || cols[j].has(val) || boxes[boxIndex].has(val)) {
+        return false;
+      }
+
+      // Fill values
+      row.add(val);
+      cols[j].add(val);
+      boxes[boxIndex].add(val);
+    }
   }
 
   return true;
 };
 
+// Test Case #1 -> true
+// const value = [
+//   ["5", "3", ".", ".", "7", ".", ".", ".", "."]
+//   , ["6", ".", ".", "1", "9", "5", ".", ".", "."]
+//   , [".", "9", "8", ".", ".", ".", ".", "6", "."]
+//   , ["8", ".", ".", ".", "6", ".", ".", ".", "3"]
+//   , ["4", ".", ".", "8", ".", "3", ".", ".", "1"]
+//   , ["7", ".", ".", ".", "2", ".", ".", ".", "6"]
+//   , [".", "6", ".", ".", ".", ".", "2", "8", "."]
+//   , [".", ".", ".", "4", "1", "9", ".", ".", "5"]
+//   , [".", ".", ".", ".", "8", ".", ".", "7", "9"]];
+// Test Case #2
 const value = [
-  ["5","3",".",".","7",".",".",".","."]
-,["6",".",".","1","9","5",".",".","."]
-,[".","9","8",".",".",".",".","6","."]
-,["8",".",".",".","6",".",".",".","3"]
-,["4",".",".","8",".","3",".",".","1"]
-,["7",".",".",".","2",".",".",".","6"]
-,[".","6",".",".",".",".","2","8","."]
-,[".",".",".","4","1","9",".",".","5"]
-,[".",".",".",".","8",".",".","7","9"]];
+  [".", ".", "4", ".", ".", ".", "6", "3", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  ["5", ".", ".", ".", ".", ".", ".", "9", "."],
+  [".", ".", ".", "5", "6", ".", ".", ".", "."],
+  ["4", ".", "3", ".", ".", ".", ".", ".", "1"],
+  [".", ".", ".", "7", ".", ".", ".", ".", "."],
+  [".", ".", ".", "5", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."]];
 const result = isValidSudoku(value);
 console.log(result);
