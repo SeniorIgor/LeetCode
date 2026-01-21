7009250289f1cca76e4da99fddd9c8d3887e4912
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2026-01-21 09:21:37 +0600

    feat: solved 54. Spiral Matrix

--- challenges/54. Spiral Matrix.ts
+++ challenges/54. Spiral Matrix.ts
@@ -1,34 +1,38 @@
 // TODO: Solve again
-function spiralOrder(matrix: number[][]): number[] {
-  let top = 0;
-  let left = 0;
-  let bottom = matrix.length - 1;
-  let right = matrix[0].length - 1;
+export function spiralOrder(matrix: number[][]): number[] {
+  const m = matrix.length;
+  const n = matrix[0].length;
 
-  const result: Array<number> = [];
-  
-  while(top <= bottom && left <= right) {
+  let top = 0;
+  let bottom = m - 1;
+  let left = 0;
+  let right = n - 1;
+
+  let result = new Array<number>(m * n);
+  let idx = 0;
+
+  while (top <= bottom && left <= right) {
     for (let i = left; i <= right; i++) {
-      result.push(matrix[top][i]);
+      result[idx++] = matrix[top][i];
     }
     top++;
-    
+
     for (let i = top; i <= bottom; i++) {
-      result.push(matrix[i][right]);
+      result[idx++] = matrix[i][right];
     }
     right--;
 
-    if(!(top <= bottom && left <= right)) {
+    if (top > bottom || left > right) {
       break;
     }
 
     for (let i = right; i >= left; i--) {
-      result.push(matrix[bottom][i]);
+      result[idx++] = matrix[bottom][i];
     }
     bottom--;
-    
+
     for (let i = bottom; i >= top; i--) {
-      result.push(matrix[i][left]);
+      result[idx++] = matrix[i][left];
     }
     left++;
   }
@@ -36,6 +40,12 @@ function spiralOrder(matrix: number[][]): number[] {
   return result;
 };
 
-const value = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+// Test Case #1 -> 
+// const value = [
+//   [1, 2, 3, 4],
+//   [5, 6, 7, 8],
+//   [9, 10, 11, 12]]
+// Test Case #2 -> [7,9,6]
+const value = [[7], [9], [6]];
 const result = spiralOrder(value);
 console.log(result);
