a43c66d5de48183e4b70daa623d5dcf1891ef7b8
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2026-01-23 11:09:30 +0600

    feat: 48. Rotate Image

--- challenges/48. Rotate Image.ts
+++ challenges/48. Rotate Image.ts
@@ -1,27 +1,54 @@
-function rotate(matrix: number[][]): void {
-  for (let i = 0; i < matrix[0].length; i++) {
-    let start = 0;
-    let end = matrix.length - 1;
-    
-    while (start < end) {
-      const temp = matrix[start][i];
-      matrix[start][i] = matrix[end][i];
-      matrix[end][i] = temp; 
-
-      start++;
-      end--;
+export function rotate(matrix: number[][]): void {
+  const n = matrix.length;
+  
+  for(let row = 0; row < n; row++) {
+    for (let column = row + 1; column < n; column++) {
+      [matrix[row][column], matrix[column][row]] = [matrix[column][row], matrix[row][column]];
     }
   }
 
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = i + 1; j < matrix[i].length; j++) {
-      const temp = matrix[i][j];
-      matrix[i][j] = matrix[j][i];
-      matrix[j][i] = temp;
-    }
+  for (let row = 0; row < n; row++) {
+    matrix[row].reverse();
   }
 };
 
+// Test Case #1
 const value = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]];
+// Test Case #2
+// const value = [[1,2,3],[4,5,6],[7,8,9]];
 const result = rotate(value);
 console.log(result, value);
+
+// [1,2,3]
+// [4,5,6]
+// [7,8,9]
+
+// [9,6,3]
+// [8,5,2]
+// [7,4,1]
+
+// [7,4,1]
+// [8,5,2]
+// [9,6,3]
+
+// Another Solution
+// export function rotate(matrix: number[][]): void {
+//   const rows = matrix.length - 1;
+//   const columns = matrix[0].length - 1;
+  
+//   for(let row = 0; row < rows; row++) {
+//     for (let column = 0; column < columns - row; column++) {
+//       const item = matrix[row][column];
+//       matrix[row][column] = matrix[rows - column][columns - row];
+//       matrix[rows - column][columns - row] = item;
+//     }
+//   }
+
+//   for (let row = 0; row <= Math.trunc(rows / 2); row++) {
+//     for (let column = 0; column <= columns; column++) {
+//       const item = matrix[row][column];
+//       matrix[row][column] = matrix[rows - row][column];
+//       matrix[rows - row][column] = item;
+//     }    
+//   }
+// };
