f33a84ddca075af5a5f144a6770a1074c223aa77
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2026-01-24 10:02:25 +0600

    feat: solved 73. Set Matrix Zeroes

--- challenges/73. Set Matrix Zeroes.ts
+++ challenges/73. Set Matrix Zeroes.ts
@@ -1,35 +1,46 @@
 // TODO: Solve again
-function setZeroes(matrix: number[][]): void {
-  let m = matrix.length;
-  let n = matrix[0].length;
-  let isColumnCell = false;
-  
+export function setZeroes(matrix: number[][]): void {
+  const m = matrix.length;
+  const n = matrix[0].length;
+
+  let firstColHasZero = false;
+
+  // First pass: mark rows and columns
   for (let i = 0; i < m; i++) {
-    if(matrix[i][0] === 0) {
-      isColumnCell = true;
+    if (matrix[i][0] === 0) {
+      firstColHasZero = true;
     }
 
     for (let j = 1; j < n; j++) {
-      if(matrix[i][j] === 0) {
+      if (matrix[i][j] === 0) {
         matrix[i][0] = 0;
         matrix[0][j] = 0;
       }
-    } 
+    }
   }
 
+  // Second pass: apply zeroes (reverse order)
   for (let i = m - 1; i >= 0; i--) {
-    for (let j = n - 1; j > 0; j--) {
-      if(matrix[i][0] === 0 || matrix[0][j] === 0) {
+    for (let j = n - 1; j >= 1; j--) {
+      if (matrix[i][0] === 0 || matrix[0][j] === 0) {
         matrix[i][j] = 0;
       }
     }
 
-    if(isColumnCell) {
+    if (firstColHasZero) {
       matrix[i][0] = 0;
     }
   }
-};
+}
 
-const value = [[0,1,2,0],[3,4,5,2],[1,3,1,5]];
+
+// Test Case #1
+// const value = [[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]];
+// Test Case #2
+// const value = [[1, 1, 1], [1, 0, 1], [1, 1, 1]];
+// Test Case #3
+// const value = [[1, 2, 3, 4], [5, 0, 7, 8], [0, 10, 11, 12], [13, 14, 15, 0]];
+// Test Case #4
+const value = [[-4, -2147483648, 6, -7, 0], [-8, 6, -8, -6, 0], [2147483647, 2, -9, -6, -10]];
 const result = setZeroes(value);
 console.log(result, value);
