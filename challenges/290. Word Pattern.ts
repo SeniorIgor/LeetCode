d647ca703299669edbd10b8bb0bf28a992bebf1a
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2026-01-28 07:22:50 +0600

    feat: solved 290. Word Pattern

--- challenges/290. Word Pattern.ts
+++ challenges/290. Word Pattern.ts
@@ -1,31 +1,36 @@
-function wordPattern(pattern: string, s: string): boolean {
-  const words = s.split(" ");
+export function wordPattern(pattern: string, s: string): boolean {
+  const wordList = s.split(" ");
 
-  if(words?.length !== pattern.length) {
+  if (wordList.length !== pattern.length) {
     return false;
   }
 
-  const pMap = new Map();
-  const wMap = new Map();
+  const charToWord = new Array<string | undefined>(26);
+  const wordToChar = new Map<string, number>();
 
   for (let i = 0; i < pattern.length; i++) {
-    const pValue = pMap.get(pattern[i]);
-    const wValue = wMap.get(words[i]);
+    const idx = pattern[i].charCodeAt(0) - 97;
+    const word = wordList[i];
 
-    if(pValue !== undefined || wValue !== undefined) {
-      if(pValue !== words[i] || wValue !== pattern[i]) {
-        return false;
-      }
-    } else {
-      pMap.set(pattern[i], words[i]);
-      wMap.set(words[i], pattern[i]);
+    const mappedWord = charToWord[idx];
+    const mappedChar = wordToChar.get(word);
+
+    if (mappedWord === undefined && mappedChar === undefined) {
+      charToWord[idx] = word;
+      wordToChar.set(word, idx);
+    } else if (mappedWord !== word || mappedChar !== idx) {
+      return false;
     }
   }
 
   return true;
 };
 
+// Test Case #1
 const value = "abba";
 const magazine = "dog cat cat fish";
+// Test Case #2
+// const value = "abba";
+// const magazine = "dog cat cat dog";
 const result = wordPattern(value, magazine);
 console.log(result);
