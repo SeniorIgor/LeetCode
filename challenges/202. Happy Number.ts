ebeef4f48e059ddc34265a73205d61a0a44eb577
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2026-02-15 09:05:51 +0600

    feat: solved 202. Happy Number

--- challenges/202. Happy Number.ts
+++ challenges/202. Happy Number.ts
@@ -1,31 +1,65 @@
 // TODO: Solve again
-function isHappy(n: number): boolean {
-  const set = new Set();
-  let num = n;
+export function isHappy(n: number): boolean {
+  let slow = n;
+  let fast = n;
 
-  while(num !== 1) {
-    set.add(num);
+  do {
+    slow = next(slow);
+    fast = next(fast);
+    fast = next(fast);
+  } while (fast !== 1 && slow !== fast)
 
-    num = digitSquareSum(num)
-
-    if(set.has(num)) {
-      return false;
-    }
-  }
-
-  return true;
+  return fast === 1;
 };
 
-function digitSquareSum(number: number) {
-  const digits = String(number).split('');
+function next(num: number) {
+  let sum = 0;
 
-  let result = digits.reduce((res, digit) => {
-    return res += (+digit * +digit);
-  }, 0);
+  while (num > 0) {
+    const digit = num % 10;
 
-  return result;
+    sum += digit * digit;
+
+    num = Math.floor(num / 10);
+  }
+
+  return sum;
 }
 
-const value = 2;
+// Test Case #1 -> true
+const value = 19;
 const result = isHappy(value);
+
+// Test Case #2 -> false
+// const value = 2;
+// const result = isHappy(value);
 console.log(result);
+
+// Another Solution
+// export function isHappy(n: number): boolean {
+//   const seen = new Set();
+
+//   let sum = n;
+
+//   while (sum !== 1 && !seen.has(sum)) {
+//     seen.add(sum);
+
+//     sum = squareDigitSum(sum);
+//   }
+
+//   return sum === 1;
+// };
+
+// function squareDigitSum(num: number) {
+//   let sum = 0;
+
+//   while (num > 0) {
+//     const digit = num % 10;
+
+//     sum += digit * digit;
+
+//     num = Math.floor(num / 10);
+//   }
+
+//   return sum;
+// }
