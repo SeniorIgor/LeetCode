0e7f50053b5b2d9e66dc94ada9696ca62ab0055d
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2026-02-19 08:46:26 +0600

    feat: solved 56. Merge Intervals

--- challenges/56. Merge Intervals.ts
+++ challenges/56. Merge Intervals.ts
@@ -1,25 +1,64 @@
-function merge(intervals: number[][]): number[][] {
-  if(intervals.length < 2) {
-    return intervals;
-  }
-  
+export function merge(intervals: number[][]): number[][] {
   intervals.sort((a, b) => a[0] - b[0]);
 
-  const result: Array<Array<number>> = [intervals[0]];
+  const merged: number[][] = [];
 
-  for (let i = 1; i < intervals.length; i++) {   
-    const lastElement = result[result.length - 1];
+  for (let [start, end] of intervals) {
+    const prev = merged[merged.length - 1];
 
-    if(lastElement[1] >= intervals[i][0]) {
-      lastElement[1] = Math.max(lastElement[1], intervals[i][1]);
-    } else {
-      result.push(intervals[i]);
+    if (prev === undefined || prev[1] < start) {
+      merged.push([start, end]);
+    } else if (end > prev[1]) {
+      merged[merged.length - 1][1] = end;
     }
   }
 
-  return result;
+  return merged;
 };
 
-const value = [[1,4],[0,0]];
+// Test Case #1 -> [ [ 0, 0 ], [ 1, 4 ] ]
+// const value = [[1, 4], [0, 0]];
+// const result = merge(value);
+// Test Case #2 -> [[1,6],[8,10],[15,18]]
+// const value = [[1, 3], [2, 6], [8, 10], [15, 18]];
+// const result = merge(value);
+// Test Case #3 -> [1,5]]
+const value = [[1, 4], [4, 5]];
 const result = merge(value);
 console.log(result);
+
+// Another Solution
+// export function merge(intervals: number[][]): number[][] {
+//   const starts = Array.from<number>({ length: 1_0000 }).fill(-1);
+
+//   for (let i = 0; i < intervals.length; i++) {
+//     const value = starts[intervals[i][0]];
+
+//     if (value < intervals[i][1]) {
+//       starts[intervals[i][0]] = intervals[i][1];
+//     }
+//   }
+
+//   const mergedArray = new Array<Array<number>>();
+
+//   for (let i = 0; i < starts.length; i++) {
+//     if (starts[i] === -1) {
+//       continue;
+//     }
+
+//     let start = i;
+//     let end = starts[i];
+
+//     while (i < end && i < starts.length) {
+//       i++;
+
+//       if (starts[i] > end) {
+//         end = starts[i];
+//       }
+//     }
+
+//     mergedArray.push([start, end]);
+//   }
+
+//   return mergedArray;
+// };
