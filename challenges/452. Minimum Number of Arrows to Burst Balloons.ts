223482c931685187be365b167c3c303e64dc1694
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2026-02-25 07:02:55 +0600

    feat: solved 452. Minimum Number of Arrows to Burst Balloons

--- challenges/452. Minimum Number of Arrows to Burst Balloons.ts
+++ challenges/452. Minimum Number of Arrows to Burst Balloons.ts
@@ -1,19 +1,41 @@
-function findMinArrowShots(points: number[][]): number {
-  let end: number | undefined; 
-  let count = 0;
-  
+export function findMinArrowShots(points: number[][]): number {
   points.sort((a, b) => a[1] - b[1]);
 
-  for (let i = 0; i < points.length; i++) { 
-    if(end === undefined || end < points[i][0]) {
-      end = points[i][1];
-      count++;
+  let arrows = 1;
+  let arrowX = points[0][1];
+
+  for (let i = 1; i < points.length; i++) {
+    const [start, end] = points[i];
+
+    if (start > arrowX) {
+      arrows++;
+      arrowX = end;
     }
   }
 
-  return count;
+  return arrows;
 };
 
-const value = [[10,16],[2,8],[1,6],[7,12]];
+const value = [[10, 16], [2, 8], [1, 6], [7, 12]];
 const result = findMinArrowShots(value);
 console.log(result);
+
+// Another Solution
+// export function findMinArrowShots(points: number[][]): number {
+//   points.sort((a, b) => a[0] - b[0]);
+//   let arrows = 1;
+//   let overlapEnd = points[0][1];
+
+//   for (let i = 1; i < points.length; i++) {
+//     const [start, end] = points[i];
+
+//     if (start <= overlapEnd) {
+//       overlapEnd = Math.min(overlapEnd, end);
+//     } else {
+//       arrows++;
+//       overlapEnd = end;
+//     }
+//   }
+
+//   return arrows;
+// };
