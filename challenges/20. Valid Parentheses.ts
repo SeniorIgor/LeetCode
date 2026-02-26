125646ad209a57ad242f0ffbc5592303538b5159
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2026-02-26 08:33:33 +0600

    feat: solved 20. Valid Parentheses

--- challenges/20. Valid Parentheses.ts
+++ challenges/20. Valid Parentheses.ts
@@ -1,23 +1,46 @@
-function isValid(s: string): boolean {
-  const stack: Array<string> = [];
+export function isValid(s: string): boolean {
+  const stack: string[] = [];
 
   for (let i = 0; i < s.length; i++) {
     const item = s[i];
 
-    if(item === '(') {
-      stack.push(')');
-    } else if(item === '[') {
-      stack.push(']');
-    } else if(item === '{') {
-      stack.push('}');
-    } else if(!stack.length || stack.pop() !== item) {
+    if (item === '(') {
+      stack.push(")");
+    } else if (item === '{') {
+      stack.push("}");
+    } else if (item === '[') {
+      stack.push("]");
+    } else if (stack.pop() !== s[i]) {
       return false;
     }
   }
 
-  return !stack.length;
+  return stack.length === 0;
 };
 
-const value = "(]";
+// Test Case #1
+// const value = "(]";
+// const result = isValid(value);
+// Test Case #2
+const value = "()";
 const result = isValid(value);
 console.log(result);
+
+// Another Solution
+// export function isValid(s: string): boolean {
+//   const expectedClose: Record<string, string> = { "(": ")", "{": "}", "[": "]" };
+//   let stack: string[] = [];
+
+//   for (let i = 0; i < s.length; i++) {
+//     const value = expectedClose[s[i]];
+
+//     if (value !== undefined) {
+//       stack.push(value);
+//       continue;
+//     } else if (stack.pop() !== s[i]) {
+//       return false;
+//     }
+//   }
+
+//   return stack.length === 0;
+// };
