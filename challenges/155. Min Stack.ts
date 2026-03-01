048004c52c95e21bfa6fda35acdc3d3d0bb9b587
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2026-03-01 09:37:35 +0600

    feat: solved 155. Min Stack

--- challenges/155. Min Stack.ts
+++ challenges/155. Min Stack.ts
@@ -1,46 +1,85 @@
-// TODO: Solve again
-class MinStack {
-  stack: Array<number>;
-  minStack: Array<number>;
+class Node {
+  constructor(
+    public val: number,
+    public min: number,
+    public next: Node | null
+  ) { }
+}
 
-  constructor() {
-    this.stack = [];
-    this.minStack = [];    
-  }
+// TODO: Solve again
+export class MinStack {
+  private head: Node | null = null;
 
   push(val: number): void {
-    this.stack.push(val);
-
-    if(!this.minStack.length) {
-      this.minStack.push(val);
+    if (this.head === null) {
+      this.head = new Node(val, val, null);
     } else {
-      const prevMin = this.minStack[this.minStack.length - 1]; 
-      
-      this.minStack.push(Math.min(prevMin, val));
+      this.head = new Node(val, Math.min(val, this.head.min), this.head);
     }
   }
 
   pop(): void {
-    this.stack.pop();
-    this.minStack.pop();
+    this.head = this.head!.next;
   }
 
   top(): number {
-    return this.stack[this.stack.length - 1];
+    return this.head!.val;
   }
 
   getMin(): number {
-    return this.minStack[this.minStack.length - 1];
+    return this.head!.min;
   }
 }
 
+// Test Case #1
+// const minStack = new MinStack();
+// minStack.push(-2);
+// minStack.push(0);
+// minStack.push(-3);
+// console.log(minStack.getMin()); // -3
+// minStack.pop();
+// console.log(minStack.top()); // 0
+// console.log(minStack.getMin()); // -2
+// Test Case #2
 const minStack = new MinStack();
-minStack.push(-2);
 minStack.push(0);
-minStack.push(-3);
-console.log(minStack.getMin());
+minStack.push(1);
+minStack.push(0);
+console.log(minStack.getMin()); // 0
 minStack.pop();
-console.log(minStack.top()); 
-console.log(minStack.getMin());
+console.log(minStack.getMin()); // 0
+minStack.pop();
+console.log(minStack.getMin()); // 0
+minStack.pop();
 
 console.log(minStack);
+
+// Another Solution
+// export class MinStack {
+//   private values: number[] = [];
+//   private mins: number[] = [];
+
+//   push(val: number): void {
+//     this.values.push(val);
+
+//     if (this.mins.length === 0) {
+//       this.mins.push(val);
+//     } else {
+//       const currentMin = Math.min(this.mins[this.mins.length - 1], val);
+//       this.mins.push(currentMin);
+//     }
+//   }
+
+//   pop(): void {
+//     this.values.pop();
+//     this.mins.pop();
+//   }
+
+//   top(): number {
+//     return this.values[this.values.length - 1];
+//   }
+
+//   getMin(): number {
+//     return this.mins[this.mins.length - 1];
+//   }
+// }
