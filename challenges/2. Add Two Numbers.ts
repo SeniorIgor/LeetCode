17c3141a1eca3b15e678c69c14601c4449092a56
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2026-03-07 08:52:30 +0600

    feat: solved 2. Add Two Numbers

--- challenges/2. Add Two Numbers.ts
+++ challenges/2. Add Two Numbers.ts
@@ -1,34 +1,32 @@
 class ListNode {
-    val: number
-    next: ListNode | null
-    constructor(val?: number, next?: ListNode | null) {
-        this.val = (val===undefined ? 0 : val)
-        this.next = (next===undefined ? null : next)
-    }
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+  }
 }
 
-function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-  let current: ListNode = new ListNode();
-  let head: ListNode = current;
-  let decimal = 0;
-  
-  while(l1 !== null || l2 !== null || decimal > 0) {
-    const sum = decimal + (l1?.val || 0) + (l2?.val || 0);
-    decimal = Math.trunc(sum / 10);
-    const rest = sum % 10;
+export function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+  const dummy = new ListNode();
+  let current = dummy;
+  let carry = 0;
 
-    const node: ListNode = new ListNode(rest);
+  while (l1 !== null || l2 !== null || carry !== 0) {
+    const sum = carry + (l1?.val ?? 0) + (l2?.val ?? 0);
+    carry = Math.trunc(sum / 10);
 
-    current.next = node;
+    current.next = new ListNode(sum % 10);
     current = current.next;
 
-    l1 = l1?.next || null;
-    l2 = l2?.next || null;
+    l1 = l1?.next ?? null;
+    l2 = l2?.next ?? null;
   }
 
-  return head.next;
+  return dummy.next;
 };
 
+// Test Case #1 -> 10998
 const first = new ListNode(9);
 const second = new ListNode(9);
 first.next = second;
@@ -42,6 +40,18 @@ const sixth = new ListNode(9);
 fifth.next = sixth;
 const seventh = new ListNode(9);
 sixth.next = seventh;
+// Test Case #2 -> 708
+// const first = new ListNode(2);
+// const second = new ListNode(4);
+// first.next = second;
+// const third = new ListNode(3);
+// second.next = third;
+
+// const fourth = new ListNode(5);
+// const fifth = new ListNode(6);
+// fourth.next = fifth;
+// const sixth = new ListNode(4);
+// fifth.next = sixth;
 
 const result = addTwoNumbers(first, fourth);
 console.log(JSON.stringify(result, null, 4));
